8300272c7e71fa439c30a9f80e59591c00713406
Rwilson2855 <rwilson2855@gmail.com>
2025-01-16 20:59:24 -0600

    you dont work yet huh

--- TodoApp/src/App.tsx
+++ TodoApp/src/App.tsx
@@ -2,8 +2,23 @@ import { useState } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([
-    
-  ])
+    {completed: true, id: 1, title: "Completed to do, yay!", owner: "Me"}
+    {completed: true, id: 1, title: "Incomplete to do, get to work!", owner: "You"}
+
+  ]);
+  const [sortBy, setSortBy] = useState("all")
+  const addTodo = (title, owner) => {
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      owner,
+      completed: false,
+    };
+    setTodos([...todos, newTodo])
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <div></div>
